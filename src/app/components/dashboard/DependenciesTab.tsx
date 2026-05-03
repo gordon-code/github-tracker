d25f4ec7de00f69a5f4a43326e7cc95a8b47ffbe
Will Gordon <wgordon@redhat.com>
2026-05-03 13:41:32 -0400

    perf(deps): removes redundant state filter in bot options memo

--- src/app/components/dashboard/DependenciesTab.tsx
+++ src/app/components/dashboard/DependenciesTab.tsx
@@ -64,7 +64,7 @@ export default function DependenciesTab(props: DependenciesTabProps) {
   }));
 
   const botOptions = createMemo<FilterChipGroupDef>(() => {
-    const logins = [...new Set(props.pullRequests.filter((pr) => pr.state === "OPEN").map((pr) => pr.userLogin))].sort();
+    const logins = [...new Set(props.pullRequests.map((pr) => pr.userLogin))].sort();
     return {
       label: "Bot",
       field: "bot",
